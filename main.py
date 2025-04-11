07b25bb7950e3b7510d375c43007e319720377e2
Daniel Solarin <soladan2000@gmail.com>
2025-04-11 09:49:48 -0900

    Implement initial API routes, settings management, and memory class

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
 
 app = FastAPI(
     title=""
 )
 
 
+
+app.mount("/assets", StaticFiles(directory="assets"), name="assets")
