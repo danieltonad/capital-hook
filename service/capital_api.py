74353723e7190e7b61607aa447bd4cf912f5730a
Daniel Solarin <soladan2000@gmail.com>
2025-04-14 23:31:17 -0900

    Add TradeMode enum and enhance settings management with trade mode functionality

--- service/capital_api.py
+++ service/capital_api.py
@@ -1,4 +1,39 @@
+import asyncio, json
 from settings import settings
 
 
-# async def 
+async def new_auth_header() -> dict:
+        try:
+            payload = json.dumps({
+            "identifier": settings.CAPITAL_IDENTITY,
+            "password": settings.CAPITAL_PASSWORD,
+            "encryptedPassword": False
+            })
+            headers = {
+                'X-CAP-API-KEY': settings.CAPITAL_API_KEY,
+                'Content-Type': 'application/json'
+            }
+            response = await settings.session.post(f"{settings.get_capital_host()}/api/v1/session", headers=headers, data=payload)
+            # print(response.status_code ,response.json())
+            header: dict = response.headers
+            CST = header.get("CST")
+            X_SECURITY_TOKEN = header.get("X-SECURITY-TOKEN")
+            # print(CST, X_SECURITY_TOKEN)
+        
+            return {'X-SECURITY-TOKEN': X_SECURITY_TOKEN, 'CST': CST}
+        except Exception as e:
+            await asyncio.sleep(100)
+            return await new_auth_header()
+            
+    
+@staticmethod
+async def portfolio_balance():
+    try:
+        header = settings.CAPITAL_AUTH_HEADER
+        response = await settings.SESSION.get(f"{settings.get_capital_host()}/api/v1/accounts", headers=header)
+        if response.status_code == 200:
+            data = response.json()
+            return data["accounts"]
+    except Exception as e:
+        # await Logger.app_log(title="PORTFOLIO_ERR", message=str(e))
+        return {}
--- settings.py
+++ settings.py
@@ -1,5 +1,6 @@
 from pydantic_settings import BaseSettings
 from httpx import AsyncClient
+from enums.trade import TradeMode
 
 
 class Settings(BaseSettings):
@@ -10,11 +11,29 @@ class Settings(BaseSettings):
         env_file = ".env"
         extra = "ignore" 
         
+    #
     CAPITAL_IDENTITY: str
     CAPITAL_PASSWORD: str
     CAPITAL_API_KEY: str
     
+    MODE: TradeMode
+    
+    CAPITAL_HOST_LIVE = "https://api-capital.backend-capital.com"
+    CAPITAL_HOST_DEMO = "https://demo-api-capital.backend-capital.com"
+    
+    TRADINGVIEW_IP_ADDRESS : list = ["52.89.214.238", "34.212.75.30", "54.218.53.128", "52.32.178.7"]
+    
     session: AsyncClient = AsyncClient()
+    
+    
+    def update_trade_mode(self, mode: TradeMode):
+        self.MODE = mode
+    
+    def get_capital_host(self) -> str:
+        if self.MODE == TradeMode.LIVE:
+            return self.CAPITAL_HOST_LIVE
+        return self.CAPITAL_HOST_DEMO
+    
         
         
 settings = Settings()
