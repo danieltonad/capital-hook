03dd3cdad682ca0a1d978a34dc13f759aaec892b
Daniel Solarin <soladan2000@gmail.com>
2025-04-16 23:51:44 -0900

    Refactor HookedTradeExecution class to include additional attributes and improve constructor definition

--- hook_trade.py
+++ hook_trade.py
@@ -1,17 +1,27 @@
 from enums.trade import TradeDirection
+import asyncio
 
 class HookedTradeExecution:
+    trade_direction: TradeDirection
+    epic: str
+    trade_amount: int
+    hook_name: str
+    profit: int
+    loss: int
+    deal_id: str
     
-    def __init__(self, trade_direction: TradeDirection, epic: str, trade_amount: int, profit: int, loss: int):
-        pass
+    
+    def __init__(self, trade_direction: TradeDirection, epic: str, trade_amount: int, profit: int, loss: int, hook_name: str):
+        self.trade_direction = trade_direction
+        self.epic = epic
+        self.trade_amount = trade_amount
+        self.hook_name = hook_name
+        self.profit = profit
+        self.loss = loss
+        self.deal_id = None
+        
+        
 
-    def execute_trade(self):
-        # Here you can add your custom logic before executing the trade
-        print("Executing trade with custom logic...")
-        
-        # Call the original trade execution method
-        self.trade_execution.execute_trade()
-        
-        # Here you can add your custom logic after executing the trade
-        print("Trade executed with custom logic.")
+    async def execute_trade(self):
+        pass
 
