b9023e1ae10c218025181227e544cea5f6a5622c
Daniel Solarin <soladan2000@gmail.com>
2025-04-17 13:05:34 -0900

    Add webhook route for TradingView integration

--- routes/api.py
+++
@@ -1,7 +0,0 @@
-from fastapi import APIRouter, BackgroundTasks, Request
-
-api = APIRouter()
-
-@api.post("webhook/trading-view", tags=["TradingView Webhook Endpoint"])
-async def tradingview_webhook_route(data, request: Request, background_task: BackgroundTasks):
-    return "Wagwan!"
