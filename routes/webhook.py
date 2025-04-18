43f479685ade2b5fa69eb6e022440c01e934e9e4
Daniel Solarin <soladan2000@gmail.com>
2025-04-18 10:19:49 -0900

    Add API and webhook routes to main application

--- routes/webhook.py
+++ routes/webhook.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter, BackgroundTasks, Request
 
-api = APIRouter()
+webhook = APIRouter()
 
-@api.post("webhook/trading-view", tags=["TradingView Webhook Endpoint"])
+@webhook.post("/trading-view", tags=["TradingView Webhook Endpoint"])
 async def tradingview_webhook_route(data, request: Request, background_task: BackgroundTasks):
     return "Wagwan!"
