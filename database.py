22a79e1f5c23c54f353b898d79f31b6e3924ea9b
Daniel Solarin <soladan2000@gmail.com>
2025-04-21 11:40:03 -0900

    Enhance database schema by adding missing fields to trades and failed_hooks tables

--- database.py
+++ database.py
@@ -7,14 +7,34 @@ async def create_connection() -> aiosqlite.Connection:
 # migrate database
 async def migrate_db():
     async with settings.DB_CONNECTION.cursor() as cursor:
+        # trades table
         await cursor.execute("""
             CREATE TABLE IF NOT EXISTS trades (
                 id TEXT PRIMARY KEY ,
                 epic TEXT NOT NULL,
+                leverage TEXT NOT NULL,
                 size REAL NOT NULL,
+                pnl REAL NOT NULL,
+                pnl_percentage REAL NOT NULL,
                 direction TEXT NOT NULL,
-                deal_id TEXT NOT NULL,
-                created_date TEXT NOT NULL
+                exit_type TEXT NOT NULL,
+                hook_name TEXT NOT NULL,
+                entry_price REAL NOT NULL,
+                exit_price REAL NOT NULL,
+                opened_at TEXT NOT NULL,
+                closed_at TEXT NOT NULL
+            )
+        """)
+        
+        # failed hooks table
+        await cursor.execute("""
+            CREATE TABLE IF NOT EXISTS failed_hooks (
+                id TEXT PRIMARY KEY,
+                epic TEXT NOT NULL,
+                hook_name TEXT NOT NULL,
+                direction TEXT NOT NULL,
+                error_message TEXT NOT NULL,
+                created_at TEXT NOT NULL
             )
         """)
         await settings.DB_CONNECTION.commit()
