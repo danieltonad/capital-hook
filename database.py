0bc47d95a5e64703c8106db416e501887a09b5e0
Daniel Solarin <soladan2000@gmail.com>
2025-04-22 03:27:38 -0900

    Add bot configuration table and implement trade history and failed hooks insertion functions

--- database.py
+++ database.py
@@ -37,4 +37,41 @@ async def migrate_db() -> None:
                 created_at TEXT NOT NULL
             )
         """)
+        
+        # bot config
+        await cursor.execute(
+            """
+            CREATE TABLE IF NOT EXISTS bot_config (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                trade_mode TEXT NOT NULL,
+                created_at TEXT NOT NULL DEFAULT CURRENT_TIMESTAMP
+                updated_at TEXT NOT NULL DEFAULT CURRENT_TIMESTAMP
+            )
+        """)
+        await settings.DB_CONNECTION.commit()
+        
+        
+        
+        
+async def saved_trade_history(trade_id: str, epic: str, leverage: str, size: float, pnl: float, pnl_percentage: float, direction: str, exit_type: str, hook_name: str, entry_price: float, exit_price: float, opened_at: str, closed_at: str) -> None:
+    async with settings.DB_CONNECTION as cursor:
+        await cursor.execute(
+            """
+            INSERT INTO trades (id, epic, leverage, size, pnl, pnl_percentage, direction, exit_type, hook_name, entry_price, exit_price, opened_at, closed_at)
+            VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)
+            """, (
+                trade_id, epic, leverage, size, pnl, pnl_percentage, direction, exit_type, hook_name, entry_price, exit_price, opened_at, closed_at
+            ))
+        await settings.DB_CONNECTION.commit()
+        
+        
+async def saved_failed_hooks(hook_id: str, epic: str, hook_name: str, direction: str, error_message: str, created_at: str) -> None:
+    async with settings.DB_CONNECTION as cursor:
+        await cursor.execute(
+            """
+            INSERT INTO failed_hooks (id, epic, hook_name, direction, error_message, created_at)
+            VALUES (?,?,?,?,?,?)
+            """, (
+                hook_id, epic, hook_name, direction, error_message, created_at
+            ))
         await settings.DB_CONNECTION.commit()
