2cc25193746fd14caf7d20ec22ba739707827ea7
Daniel Solarin <soladan2000@gmail.com>
2025-04-26 11:43:05 -0900

    Add dashboard view and layout; implement basic HTML structure and styles

--- routes/view.py
+++ routes/view.py
@@ -1,1 +1,18 @@
 from fastapi import APIRouter, Request
+from starlette.templating import Jinja2Templates, _TemplateResponse
+
+
+view = APIRouter()
+templates = Jinja2Templates(directory="views")
+
+
+
+@view.get("/", tags=["Dashboard"])
+async def dashboard_view(request: Request) -> _TemplateResponse:
+    """
+    Dashboard view
+    """
+    data = {
+        "positions": {}
+    }
+    return templates.TemplateResponse("pages/index.html", {"request": request, "data": data})
