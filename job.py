5bd5311ec4706f6e1b1fae0fc84f6c14f1c5637e
Daniel Solarin <soladan2000@gmail.com>
2025-04-30 09:49:10 -0900

    Initialize job scheduling in startup event; add missing newline in job.py

--- job.py
+++ job.py
@@ -1,5 +1,6 @@
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
 from service.capital_socket import capital_socket
+from service.capital_api import update_auth_header
 
 
 class Jobs:
@@ -7,8 +8,16 @@ class Jobs:
     async def run(self):
         scheduler = AsyncIOScheduler()
         
+        # ping socket
         scheduler.add_job(capital_socket.ping_socket, "interval", minutes=5)
+        # update auth header
+        scheduler.add_job(update_auth_header, "interval", minutes=5)
         # start schd
         scheduler.start()
         for job in scheduler.get_jobs():
             print("Next Job: ", job.next_run_time)
+            
+            
+
+
+jobs = Jobs()
