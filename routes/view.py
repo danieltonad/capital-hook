34d19128cad21ac4862fedb59e18cc848a415f0a
Daniel Solarin <soladan2000@gmail.com>
2025-06-12 04:21:26 -0900

    Refactor dashboard routes; add config and history views, update navigation links for improved routing.

--- routes/view.py
+++ routes/view.py
@@ -9,10 +9,21 @@ templates = Jinja2Templates(directory="views")
 
 @view.get("/", tags=["Dashboard"])
 async def dashboard_view(request: Request) -> _TemplateResponse:
-    """
-    Dashboard view
-    """
     data = {
         "positions": {}
     }
     return templates.TemplateResponse("pages/index.html", {"request": request, "data": data})
+
+@view.get("/config", tags=["Config"])
+async def dashboard_view(request: Request) -> _TemplateResponse:
+    data = {
+        "positions": {}
+    }
+    return templates.TemplateResponse("pages/config.html", {"request": request, "data": data})
+
+@view.get("/history", tags=["Trade History"])
+async def dashboard_view(request: Request) -> _TemplateResponse:
+    data = {
+        "positions": {}
+    }
+    return templates.TemplateResponse("pages/history.html", {"request": request, "data": data})
