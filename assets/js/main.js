75ca728bb7a58e1a30f2f5b84c89349a51038689
danieltonad <soladan2000@gmail.com>
2025-07-11 23:26:49 +0100

    Enhance clipboard functionality: add toast notification for payload copy and update config form to handle clipboard actions

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,3 +9,23 @@ document.addEventListener("DOMContentLoaded", function () {
 
   document.head.appendChild(lucideScript);
 });
+
+
+document.body.addEventListener('htmx:afterSwap', function(evt) {
+  if(evt.target.id === "payload-response") {
+    navigator.clipboard.writeText(evt.target.innerText);
+    showToast("Payload copied to clipboard");
+  }
+});
+
+
+function showToast(message) {
+  const toast = document.querySelector('.notify-toast');
+  const notifyText = document.querySelector('.notify-text');
+  notifyText.textContent = message;
+  toast.classList.add('show');
+
+  setTimeout(() => {
+    toast.classList.remove('show');
+  }, 3000);
+}
